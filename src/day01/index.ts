365db993ae46c08b9ee49c069fb4ce00e2ef36be
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-12-01 13:06:15 -0500

    day 1 part 2

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,4 +1,5 @@
 import run from "aocrunner";
+import { maxHeaderSize } from "http";
 
 const parseInput = (rawInput: string) => rawInput;
 
@@ -22,10 +23,25 @@ const part1 = (rawInput: string): number => {
   return largestSum;
 };
 
-const part2 = (rawInput: string) => {
+const part2 = (rawInput: string): number => {
   const input = parseInput(rawInput);
+  let topThree: number[] = [];
+  let currentSum: number = 0;
 
-  return;
+  let lines = input.split("\n").concat("");
+  lines.forEach((line, idx) => {
+    if (line.length > 0) {
+      currentSum += parseInt(line);
+    } else {
+      topThree.push(currentSum);
+      currentSum = 0;
+    }
+  });
+
+  return topThree
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, b) => a + b);
 };
 
 run({
@@ -55,10 +71,51 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        1000
+
+        2000
+
+        1000
+        2000
+        3000
+        `,
+        expected: 9000,
+      },
+      {
+        input: `
+        1000
+        2000
+        3000
+        
+        4000
+        
+        5000
+        6000
+        
+        7000
+        8000
+        9000
+        
+        10000
+        `,
+        expected: 45000,
+      },
+      {
+        input: `
+        1000
+        3000
+
+        5000
+        6000
+
+        2000
+
+        3000
+        `,
+        expected: 18000,
+      },
     ],
     solution: part2,
   },
