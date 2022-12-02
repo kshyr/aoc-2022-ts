a6395da81e0a04d2497b307842fc40aac571ee34
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-12-02 00:38:40 -0500

    day 2

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -2,34 +2,139 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput;
 
-const part1 = (rawInput: string) => {
+const part1 = (rawInput: string): number => {
   const input = parseInput(rawInput);
+  let score = 0;
 
-  return;
+  let lines = input.split(/\r?\n/);
+  lines.forEach((line) => {
+    let round = line.split(" ");
+    let roundScore = 0;
+    switch (round[0]) {
+      case "A":
+        switch (round[1]) {
+          case "X":
+            roundScore = 1 + 0;
+            break;
+          case "Y":
+            roundScore = 2 + 3;
+            break;
+          case "Z":
+            roundScore = 3 + 6;
+            break;
+        }
+        break;
+      case "B":
+        switch (round[1]) {
+          case "X":
+            roundScore = 1 + 0;
+            break;
+          case "Y":
+            roundScore = 2 + 3;
+            break;
+          case "Z":
+            roundScore = 3 + 6;
+            break;
+        }
+        break;
+      case "C":
+        switch (round[1]) {
+          case "X":
+            roundScore = 1 + 0;
+            break;
+          case "Y":
+            roundScore = 2 + 3;
+            break;
+          case "Z":
+            roundScore = 3 + 6;
+            break;
+        }
+        break;
+      }
+    score += roundScore;
+  });
+  return score;
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  let score = 0;
 
-  return;
+  let lines = input.split(/\r?\n/);
+  lines.forEach((line) => {
+    let round = line.split(" ");
+    let roundScore = 0;
+    switch (round[0]) {
+      case "A":
+        switch (round[1]) {
+          case "X":
+            roundScore = 3 + 0;
+            break;
+          case "Y":
+            roundScore = 1 + 3;
+            break;
+          case "Z":
+            roundScore = 2 + 6;
+            break;
+        }
+        break;
+      case "B":
+        switch (round[1]) {
+          case "X":
+            roundScore = 1 + 0;
+            break;
+          case "Y":
+            roundScore = 2 + 3;
+            break;
+          case "Z":
+            roundScore = 3 + 6;
+            break;
+        }
+        break;
+      case "C":
+        switch (round[1]) {
+          case "X":
+            roundScore = 2 + 0;
+            break;
+          case "Y":
+            roundScore = 3 + 3;
+            break;
+          case "Z":
+            roundScore = 1 + 6;
+            break;
+        }
+        break;
+      }
+    score += roundScore;
+  });
+  return score;
+
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        A Y
+        B X
+        C Z
+        `,
+        expected: 15,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        A Y
+        B X
+        C Z
+        `,
+        expected: 12,
+      },
     ],
     solution: part2,
   },
